15f22c7f13169e3a20bed43d8b7ea114f2e938ba
Kirill Vinokurov <kvinokurov@gmail.com>
2019-03-15 19:31:02 +0300

    Add sample windows service application

---
+++ ExampleWindowsService/IHostedServiceAsAService/ConsoleLogger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace IHostedServiceAsAService
+{
+    internal class ConsoleLogger : ILogger
+    {
+        public void Info(string str)
+        {
+            WriteLine($"{DateTime.Now:G} -> {str}");
+        }
+
+        public void InportantInfo(string str)
+        {
+            WriteLine(str, ConsoleColor.Green);
+        }
+
+        public void Warning(string str)
+        {
+            WriteLine($"Warning: {str}", ConsoleColor.Yellow);
+        }
+
+        public void Error(string str)
+        {
+            WriteLine($"ERROR: {str}", ConsoleColor.Red);
+        }
+
+        private static void WriteLine(string str, ConsoleColor? color = null)
+        {
+            ConsoleColor currentColor = default;
+            if (color.HasValue)
+            {
+                currentColor = Console.ForegroundColor;
+                Console.ForegroundColor = color.Value;
+            }
+
+            Console.WriteLine(str);
+
+            if (color.HasValue)
+                Console.ForegroundColor = currentColor;
+        }
+    }
+}
---
+++ ExampleWindowsService/IHostedServiceAsAService/ILogger.cs
@@ -0,0 +1,10 @@
+﻿namespace IHostedServiceAsAService
+{
+    internal interface ILogger
+    {
+        void Info(string str);
+        void InportantInfo(string str);
+        void Warning(string str);
+        void Error(string str);
+    }
+}
